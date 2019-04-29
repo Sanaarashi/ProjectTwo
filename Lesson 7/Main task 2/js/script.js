fc0112d0e47a99b319902da2bb989d04bcf17d62
Dmitry Zharikov <xenoffex@gmail.com>
2019-04-29 19:54:53 +0300

    second commit

---
+++ Lesson 7/Main task 2/js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let div = document.createElement('div'),
+    timeNow = new Date(),
+    hours = timeNow.getHours(),
+    mins = timeNow.getMinutes(),
+    secs = timeNow.getSeconds();
+
+if (hours < 10) {
+    hours = "0" + hours;
+}
+
+if (mins < 10) {
+    mins = "0" + mins;
+}
+
+if (secs < 10) {
+    secs = "0" + secs;
+}
+
+div.innerHTML = hours + ":" + mins + ":" + secs;
+document.body.appendChild(div);
