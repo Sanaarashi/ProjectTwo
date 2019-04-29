c9eb2b144cd9bb9fe3d1419fe7243a48f636ad5e
Dmitry Zharikov <xenoffex@gmail.com>
2019-04-29 21:16:25 +0300

    third commit

--- Lesson 7/Main task 2/js/script.js
+++ Lesson 7/Main task 2/js/script.js
@@ -1,22 +1,26 @@
 'use strict';
 
-let div = document.createElement('div'),
-    timeNow = new Date(),
-    hours = timeNow.getHours(),
-    mins = timeNow.getMinutes(),
-    secs = timeNow.getSeconds();
+let div = document.createElement('div');
 
-if (hours < 10) {
-    hours = "0" + hours;
+function showTime() {
+    let timeNow = new Date(),
+        hours = timeNow.getHours(),
+        mins = timeNow.getMinutes(),
+        secs = timeNow.getSeconds();
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (mins < 10) {
+        mins = "0" + mins;
+    }
+
+    if (secs < 10) {
+        secs = "0" + secs;
+    }
+    div.innerHTML = hours + ":" + mins + ":" + secs;
+    document.body.appendChild(div);
 }
 
-if (mins < 10) {
-    mins = "0" + mins;
-}
-
-if (secs < 10) {
-    secs = "0" + secs;
-}
-
-div.innerHTML = hours + ":" + mins + ":" + secs;
-document.body.appendChild(div);
+let autoReload = setInterval(showTime, 1000);
