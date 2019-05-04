ba4fc465fca1f9743ffe1f53b4341b9cd1be5212
Dmitry Zharikov <xenoffex@gmail.com>
2019-05-04 16:12:26 +0300

    a new commit

---
+++ Lesson 9/Main task two/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    let age = document.getElementById('age');
+
+
+    function showUser(surname, name) {
+        alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+    }
+
+    showUser.apply(age, ['Петров','Василий']);
+
+});
---
+++ Lesson 9/Main task/js/script.js
@@ -0,0 +1,123 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    'use sctrict';
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i ++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    //timer
+
+    let deadline = new Date('2019-05-27 00:10:00');
+
+    function getTimeRemaining(endtime) {
+        let time = Date.parse(endtime) - Date.parse(new Date()),
+            secs = Math.floor((time/1000) % 60),
+            mins = Math.floor((time/1000/60) % 60),
+            hours = Math.floor((time/(1000*60*60)));
+
+            
+
+        
+        return {
+            'total' :  time,
+            'hours' : hours,
+            'minutes' : mins,
+            'seconds' : secs
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateTime, 1000);
+
+        function updateTime() {
+            let time = getTimeRemaining(endtime);
+            
+            if (time.seconds < 10) {
+                time.seconds = "0" + time.seconds;
+            }
+            
+            if (time.minutes < 10) {
+                time.minutes = "0" + time.minutes;
+            }
+
+            if (time.hours < 10) {
+                time.hours = "0" + time.hours;
+            }
+
+            hours.textContent = time.hours;
+            minutes.textContent = time.minutes;
+            seconds.textContent = time.seconds;
+
+            if (time.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+            }
+        }
+    }
+    
+    //modal window
+    let moreBtn = document.querySelector('.more'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close'),
+        descBtns = document.querySelectorAll('.description-btn');
+        
+    moreBtn.addEventListener('click', () => {
+        showOverlay();
+    });
+
+    close.addEventListener('click', () => {
+        overlay.style.display = 'none';
+        moreBtn.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    descBtns.forEach( (elem) => {
+        elem.addEventListener('click', () => {
+            showOverlay();
+        });
+    });
+
+    function showOverlay() {
+        document.body.style.overflow = 'hidden';
+        overlay.style.display = 'block';
+        this.classList.add('more-splash');
+    }
+
+    setClock('timer', deadline);
+});
