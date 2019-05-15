a8be2b0bf2a067462ef143fc2b29ed1447598070
Dmitry Zharikov <xenoffex@gmail.com>
2019-05-15 23:44:40 +0300

    one more commit

--- Lesson 14/Main task/js/script.js
+++ Lesson 14/Main task/js/script.js
@@ -9,10 +9,8 @@ $(document).ready(function () {
             $('.modal').animate({opacity: 0, height:'hide'}, 1000);
         };
 
-    $('a[href="#sheldure"]').on('click', showModal);
-    $('.main_btna').on('click', showModal);
-    $('.main_btn').on('click', showModal);
-    $('.close').on('click', closeModal);
+    $('a[href="#sheldure"], .main_btna, .main_btn').on('click', showModal);
+    $('.close, .overlay').on('click', closeModal);
 
     $('.form').submit(function() { $.post('server.php', $('.form').serialize()); return false;});
 });
